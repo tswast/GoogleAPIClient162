b5e3245291a1f41585c26d76d9548dfbac35eec7
Steven Sheldon <steven@sasheldon.com>
2016-03-10 17:22:46 -0800

    Empty project.

---
+++ PodsTest/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  PodsTest
+//
+//  Created by Steven Sheldon on 3/10/16.
+//  Copyright © 2016 Steven Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ PodsTest/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PodsTest
+//
+//  Created by Steven Sheldon on 3/10/16.
+//  Copyright © 2016 Steven Sheldon. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // Override point for customization after application launch.
+  return YES;
+}
+
+@end
